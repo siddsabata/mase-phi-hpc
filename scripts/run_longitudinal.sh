5d658b51a87f9e9b26735c47690fcde328185d6c
siddsabata <siddharth@sabata.net>
2025-09-08 09:16:12 -0400

    fixing sbatch script

--- scripts/run_longitudinal.sh
+++ scripts/run_longitudinal.sh
@@ -4,7 +4,8 @@
 #SBATCH --error=longitudinal_%j.err
 #SBATCH --time=02:00:00
 #SBATCH --mem=16G
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
+#SBATCH --partition=pool1
 
 # Run longitudinal analysis
 conda run -n mase_phi_hpc python ../src/longitudinal/longitudinal_main.py --config "$1"
